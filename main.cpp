2785682555059042e60c232c9e4fc42f4e113e1e
esalini22 <95315753+esalini22@users.noreply.github.com>
2022-05-20 18:07:53 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
-#include <stdio.h>
+include <stdio.h>
 #include <string.h>
+#include <string>
 #include <fstream>
 #include <unistd.h>
-#include <pthread.h>
+#include <cmath>
+#include <omp.h>
+#include <thread>
+#include <vector>
+#include <algorithm>
 #include "HyperLogLog.h"
 using namespace std;
 typedef unsigned long long int ullint;
@@ -30,23 +35,26 @@ ullint BITS;
 	printf("kmer: %s\n",kmer);
 }*/
 
-void* leerA(void *filename){
+void leer(char *genome,int i){
 	char c; //para lectura
 	ullint kmer=0,comp=0;
 	string linea;
-	ifstream indata((char*)filename);
+	ifstream indata(genome);
 	if(!indata){
-		printf("No se pudo abrir el archivo %s\n",(char*)filename);
+		printf("No se pudo abrir el archivo %s\n",genome);
 		exit(1);
 	}
+
+	/*vector<ullint>::iterator sketch=*/hll->addSketch(genome,i); //añade el sketch inicializado con 0s
+
 	getline(indata,linea); //salta primera linea
 	getline(indata,linea); //para primer kmer
 	string::iterator it=linea.begin();
 
-	//al leerse cada caracter, se inserta la base al final del kmer no canonico
-	//y luego se inserta su complemento al inicio del complemento del reverso del kmer
+	//al leerse cada caracter, se insert la base al final del kmer no canonico
+	//y luego se insert su complemento al inicio del complemento del reverso del kmer
 	//el inicio estara dado por el largo del kmer
-	for(unsigned char i=0;i<k;++i){ //inicializacion - primer kmer
+	for(unsigned char j=0;j<k;++j){ //inicializacion - primer kmer
 		kmer=kmer<<2;
 		comp=comp>>2;
 		c=*it;
@@ -62,7 +70,7 @@ void* leerA(void *filename){
 		}
 		else if(c=='T') kmer=kmer|0x3; //T=11
 	}
-	(kmer>comp) ? hll->insertA(comp) : hll->insertA(kmer); //inserta kmer canonico
+	(kmer>comp) ? hll->insert(comp,i) : hll->insert(kmer,i); //insert kmer canonico
 
 	while(!indata.eof()){
 		while(it!=linea.end()){
@@ -81,7 +89,7 @@ void* leerA(void *filename){
 				}
 				else if(c=='T') kmer=kmer|0x3; //T=11
 
-				(kmer>comp) ? hll->insertA(comp) : hll->insertA(kmer); //inserta kmer canonico
+				(kmer>comp) ? hll->insert(comp,i) : hll->insert(kmer,i); //insert kmer canonico
 			}
 			else if(c=='>') break;
 			++it;
@@ -90,96 +98,79 @@ void* leerA(void *filename){
 		it=linea.begin();
 	}
 	indata.close();
-	return NULL;
 }
 
-void* leerB(void *filename){
-	//lee segundo archivo
-	char c; //para lectura
-	ullint kmer=0,comp=0;
-	string linea;
-	ifstream indata((char*)filename);
+//obtiene archivos de un txt
+//formato de archivos:
+//genoma1
+//genoma2
+//etc. (1 genoma por linea)
+vector<string> readFromFile(char* paths){
+	//printf("archivo: %s\n",paths);
+	ifstream indata(paths);
 	if(!indata){
-		printf("No se pudo abrir el archivo %s\n",(char*)filename);
+		printf("No se pudo abrir el archivo %s\n",paths);
 		exit(1);
 	}
-	getline(indata,linea); //salta primera linea
-	getline(indata,linea); //para primer kmer
-	string::iterator it=linea.begin();
-
-	for(unsigned char i=0;i<k;++i){ //inicializacion - primer kmer
-		kmer=kmer<<2;
-		comp=comp>>2;
-		c=*it;
-		++it;
-		if(c=='A') comp=comp|bits_T; //A=00 0xC000000000
-		else if(c=='C'){ //C=01
-			kmer=kmer|0x1;
-			comp=comp|bits_G; //0x8000000000
-		}
-		else if(c=='G'){ //G=10
-			kmer=kmer|0x2;
-			comp=comp|bits_C; //0x4000000000
-		}
-		else if(c=='T') kmer=kmer|0x3; //T=11
-	}
-	(kmer>comp) ? hll->insertB(comp) : hll->insertB(kmer);
-
+	vector<string> genomes;
+	string filename;
 	while(!indata.eof()){
-		while(it!=linea.end()){
-			c=*it;
-			if(c=='A' || c=='C' || c=='G' || c=='T'){ //lee nueva base
-				kmer=(kmer<<2)&BITS; //desplaza bases a la izquierda
-				comp=comp>>2; //desplaza bases a la derecha
-				if(c=='A') comp=comp|bits_T; //A=00
-				else if(c=='C'){ //C=01
-					kmer=kmer|0x1;
-					comp=comp|bits_G;
-				}
-				else if(c=='G'){ //G=10
-					kmer=kmer|0x2;
-					comp=comp|bits_C;
-				}
-				else if(c=='T') kmer=kmer|0x3; //T=11
-				(kmer>comp) ? hll->insertB(comp) : hll->insertB(kmer);
-			}
-			else if(c=='>') break;
-			++it;
-		}
-		getline(indata,linea);
-		it=linea.begin();
+		getline(indata,filename);
+		genomes.push_back(filename);
 	}
+	if(genomes.back()=="") genomes.pop_back(); //elimina el EOF
 	indata.close();
-	return NULL;
+	return genomes;
 }
 
+//obtiene archivos de la linea de argumentos
+vector<string> getPaths(char** argv, int argc){
+	vector<string> genomes;
+	for(int i=1;i<argc;++i){
+		if(!strcmp(argv[i],"-k") || !strcmp(argv[i],"-p")) ++i;
+		else if(!strcmp(argv[i],"-s")) genomes.push_back(argv[i]);
+	}
+	return genomes;
+}
+
+//formato: ./hll -opcion valor genomas
+//o bien ./hll genomas -opcion valor
+//no detecta caso en que se introduza opcion o valor invalido
 int main(int argc, char *argv[]){
-	//se trabaja a nivel de bits, es mas rapido que trabajar con strings
 	if(argc<3) {
 		printf("No hay suficientes argumentos");
 		exit(1);
 	}
-	unsigned char p=21;
-	if(argc==5 || argc==6){ //1 opcion: k o p
-		char arg=atoi(argv[4]);
-		if(!strcmp(argv[3],"k") && arg<32 && arg>19){
-			printf("opcion k\n");
-			k=arg;
-		}
-		else if(!strcmp(argv[3],"p") && arg<32 && arg>8){
-			printf("opcion p\n");
-			p=arg;
-		}
+	unsigned char p=22;
+	char** option;
+	char** end=argv+argc;
+	option=std::find((char**)argv,end,(const std::string&)"-k"); //cambia largo de kmer
+	if(option!=end){
+		char val=atoi(*(option+1));
+		if(val<32 && val>19) k=val;
 	}
-	else if(argc>6){ //2 opciones: k y p
-		char arg1=atoi(argv[4]);
-		char arg2=atoi(argv[6]);
-		if(!strcmp(argv[3],"k") && arg1<32 && arg1>19) k=arg1;
-		else if(!strcmp(argv[3],"p") && arg1<32 && arg1>8) p=arg1;
-		if(!strcmp(argv[5],"k") && arg2<32 && arg2>19) k=arg2;
-		else if(!strcmp(argv[5],"p") && arg2<32 && arg2>8) p=arg2;
+	option=std::find((char**)argv,end,(const std::string&)"-p"); //cambia tamaño de sketch
+	if(option!=end){
+		char val=atoi(*(option+1));
+		if(val<32 && val>8) p=val;
 	}
-	hll = new HyperLogLog(p,32-p);
+	vector<string> genomes;
+	option=std::find((char**)argv,end,(const std::string&)"-f"); //lee de txt
+	if(option!=end) genomes=readFromFile(*(option+1));
+	else genomes=getPaths(argv,argc);
+	int tam=genomes.size();
+
+	for(vector<string>::iterator it=genomes.begin();it!=genomes.end();++it)
+		printf("%s ",(char*)(*it).c_str());
+
+	printf("\nk: %d p: %d\n",k,p);
+	
+
+	int numThreads=min(tam,(int)std::thread::hardware_concurrency());
+	printf("threads: %d\n",numThreads);
+	hll = new HyperLogLog(p,32-p,k,numThreads,tam);
+
+	//se trabaja a nivel de bits, es mas rapido que trabajar con strings
 	//aca se determina como se insertaran las bases complementarias en el complemento del reverso del kmer
 	//es decir, se determina donde esta el inicio (en bits) de dicho kmer
 	const ullint desp=(2*(k-1));
@@ -190,12 +181,20 @@ int main(int argc, char *argv[]){
 	BITS=(bits_C-1)<<2;
 
 	//lee paralelamente cada archivo
-	pthread_t ids[2]; //arreglo de hebras
-	pthread_create(&ids[0], NULL, leerA, argv[1]);
-	pthread_create(&ids[1], NULL, leerB, argv[2]);
-	
-	pthread_join(ids[0], NULL);
-	pthread_join(ids[1], NULL);
+	omp_set_num_threads(numThreads);
+	#pragma omp parallel
+	{
+		#pragma omp single
+		{
+			for(int i=0;i<tam;i++){
+				#pragma omp task
+				leer((char*)genomes[i].c_str(),i);
+			}
+		}
+	}
+	option=std::find((char**)argv,end,(const std::string&)"-s"); //guarda los sketches
+	if(option!=end) hll->saveSketches();
+	hll->estCard();
 	hll->estJaccard();
 	delete hll;
 	return 0;
