363d7672ff73e5dc12f31a5064a139399463ef7f
esalini22 <95315753+esalini22@users.noreply.github.com>
2022-05-20 18:07:24 -0400

    Update HyperLogLog.h

--- HyperLogLog.h
+++ HyperLogLog.h
@@ -1,6 +1,8 @@
 #ifndef HYPERLOGLOG_H
 #define HYPERLOGLOG_H
 #include <vector>
+//#include <unordered_map>
+#include <string>
 typedef unsigned long long int ullint;
 using namespace std;
 class HyperLogLog{
@@ -9,15 +11,22 @@ class HyperLogLog{
         unsigned int bits_v2; //bits para hacer & en operacion bitwise
         long double N; //corresponde a |M|: numero de buckets - se usa long double para evitar hacer casting al calcular cardinalidad
         long double a_m; //factor de correccion * N^2
-        unsigned int cerosA,cerosB,cerosU;
+        string kmer_length;
+        string sketch_size;
         vector<ullint> bit_mask; //vector de máscaras de bits, usadas para insertar registros
-        vector<ullint> sketchA,sketchB; //se usa vector de ullint, cada celda (64 bits) a su vez tiene hasta 12 buckets
-        vector<unsigned int> wA,wB,wU; //vectores de contadores de w (elementos en sketches)
+        //se usa vector de ullint, cada celda (64 bits) a su vez tiene hasta 12 buckets
+        vector<vector<ullint>> sketch; //n sketches, n: numero de genomas
+        vector<string> names; //vector con los nombres de los genomas
+        vector<long double> cards; //cardinalidades individuales, para cálculo de jaccard
+        //unordered_map<string,long double> jaccards; //mapa con jaccard de pares, para matriz de dist
     public:
-        HyperLogLog(unsigned char n1,unsigned char n2);
+        HyperLogLog(unsigned char n1,unsigned char n2,unsigned char k,int numThreads,int n);
         ~HyperLogLog();
-        void insertA(ullint kmer);
-        void insertB(ullint kmer);
-        void estJaccard();
+        void addSketch(string genome,int i); //añade un sketch vacio al mapa de sketches, para funciones hll y dist
+        void insert(ullint kmer,int i); //inserta en sketch, para funciones hll y dist
+        void loadSketch(char* filename); //carga un archivo de sketch en memoria, para hll y dist
+        void saveSketches(); //guarda el sketch individual en un archivo, para funcion sketch
+        void estCard(); //estima cardinalidad individual, para funciones hll y dist
+        void estJaccard(); //estima jaccard, para funcion dist
 };
 #endif
