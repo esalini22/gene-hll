2a39621c818f5d74ed917806a3f3549bb151cc95
esalini22 <95315753+esalini22@users.noreply.github.com>
2022-09-12 14:16:21 -0300

    Update HyperLogLog.cpp

--- HyperLogLog.cpp
+++ HyperLogLog.cpp
@@ -8,7 +8,7 @@
 #include "wyhash32.h"
 
 using namespace std;
-#define seed 5 //seed para hash
+#define seed 5 //seed para hash - CAMBIAR PARA VER COMO CAMBIA TIEMPO Y PRECISION
 #define lim 4294967296 //2^32
 #define lim_int 2147483647
 
@@ -19,8 +19,8 @@ HyperLogLog::HyperLogLog(unsigned char n1, unsigned char n2,unsigned char k,int 
 	bits_v2=(1<<b)-1; //2^b -1
 	N=1<<p; //2^V1
 	a_m=(0.7213/(1+(1.079/N)))*N*N;
-	for(unsigned char i=0;i<12;++i)
-		bit_mask.emplace_back(~((ullint)0x1F<<(5*i)));
+	for(unsigned char i=0;i<16;++i)
+		bit_mask.emplace_back(~((ullint)0xF<<(4*i)));
 	cards.resize(n);
 	sketch.resize(n);
 	names.resize(n);
@@ -28,14 +28,15 @@ HyperLogLog::HyperLogLog(unsigned char n1, unsigned char n2,unsigned char k,int 
 	sketch_size=to_string(p);
 	jaccards.resize(n);
 	ciclos_red=(b+2)/8+(((b+2)%8)>0);
+	min_val=1<<(b-15);
 }
 HyperLogLog::~HyperLogLog(){}
 
 void HyperLogLog::addSketch(string genome,int i){
 	//inicializa sketch en 0s, y añade su nombre a un vector
-	for(unsigned int j=0;j<N/12;++j) //cada celda tendra 12 buckets del sketch array
+	for(unsigned int j=0;j<N/16;++j) //cada celda tendra 12 buckets del sketch array
 		sketch[i].emplace_back(0);
-	if((long)N%12)
+	if((long)N%16)
 		sketch[i].emplace_back(0);
 	names[i]=genome;
 }
@@ -46,21 +47,23 @@ void HyperLogLog::insert(ullint kmer,int i){
 	uint32_t hash=wyhash32(key,8,seed);
 
 	unsigned int v1=hash>>b; //se desplaza el hash b bits a la derecha
-	ullint v2=hash&bits_v2; // AND 2^b -1
-	if(v2==0) v2=b+1;
+	unsigned int v2=hash&bits_v2; // AND 2^b -1 
+	if(v2<min_val) v2=15;
 	else v2=__builtin_clz(v2)+1-p; //cuenta cantidad de bits 0 mas significativo
 	//lo cual sirve para encontrar posicion de 1 mas a la izquierda
 	//se resta p, ya que al trabajar con int, se tiene 32 bits,
 	//y siempre habran al menos p ceros a la izquierda
-	unsigned int indice=v1/12; //indice de celda
-	unsigned char bucket=v1%12; //12 buckets por celda
-	ullint temp=(sketch[i][indice]>>(5*bucket))&0x1F;
-	if(v2 > temp) sketch[i][indice]=(sketch[i][indice]&(bit_mask[bucket]))|(v2<<(5*bucket));
+	unsigned int indice=v1/16; //indice de celda
+	unsigned char bucket=v1%16; //12 buckets por celda
+	ullint temp=(sketch[i][indice]>>(4*bucket))&0xF;
+	if(v2 > temp) sketch[i][indice]=(sketch[i][indice]&(bit_mask[bucket]))|((ullint)v2<<(4*bucket));
 }
 
 void HyperLogLog::loadSketch(char *infilename,int i){ //¿que pasa si tamaño del sketch es distinto al de argumento?
 	string genome=infilename; //se debe determinar nombre del genoma a partir del archivo .hll
+	printf("infilename: %s\n",infilename);
 	genome.erase(genome.find(".w."),string::npos);
+	printf("genome: %s\n",genome.c_str());
 
 	//debemos descomprimir el archivo y ponerlo en un vector
 	vector<ullint> v;
@@ -84,9 +87,10 @@ void HyperLogLog::loadSketch(char *infilename,int i){ //¿que pasa si tamaño de
 	if(cont>0) v.emplace_back(strtoull(temp,&end,10));
 	v.emplace_back(0);
 	gzclose(infile);
-	
+
 	sketch[i]=v;
 	names[i]=genome;
+	printf("done\n");
 }
 
 void HyperLogLog::saveSketches(){ //usar zlib para comprimir
@@ -161,33 +165,36 @@ inline float HyperLogLog::hsum_avx(__m256 v) {
 }
 
 void HyperLogLog::estCard(){
-	for(int i=0;i<ciclos_red;++i)
-		vect[i]=_mm256_loadu_ps((const float *)&wt[i*8]);
-
 	__m256i vec3; //vector de mascaras de bits
-	ullint bits_and[4]={31,31,31,31};
+	ullint bits_and[4]={15,15,15,15};
+	//vec3=_mm256_loadu_epi64((void const *)&bits_and[0]);
 	vec3=_mm256_loadu_si256((const __m256i*)&bits_and[0]);
 
 	int tam=sketch.size();
 	#pragma omp parallel for
 	for (int j = 0; j < tam; ++j){
+		FILE *fp = fopen((names[j]+".sketch").c_str(),"w");
+
 		int ceros=N;
 		vector<float> w(b+2,0);
 		vector<ullint>::iterator it1=sketch[j].begin();
 		vector<ullint>::iterator fin=sketch[j].end();
 		//contamos las repeticiones de w en cada sketch
-		while(it1!=fin-1){ 
+		while(it1!=fin){ 
 			ullint i1=*it1;
-			ullint it_array[12]={i1,(i1>>5),(i1>>10),(i1>>15),(i1>>20),(i1>>25),(i1>>30),(i1>>35),(i1>>40),(i1>>45),(i1>>50),(i1>>55)};
-			__m256i vec4[3];
+
+			ullint it_array[16]={i1,(i1>>4),(i1>>8),(i1>>12),(i1>>16),(i1>>20),(i1>>24),(i1>>28),(i1>>32),(i1>>36),(i1>>40),(i1>>44),(i1>>48),(i1>>52),(i1>>56),(i1>>60)};
+			__m256i vec4[4];
 			vec4[0]=_mm256_loadu_si256((const __m256i*)&it_array[0]);
 			vec4[1]=_mm256_loadu_si256((const __m256i*)&it_array[4]);
 			vec4[2]=_mm256_loadu_si256((const __m256i*)&it_array[8]);
+			vec4[3]=_mm256_loadu_si256((const __m256i*)&it_array[12]);
 			vec4[0]=_mm256_and_si256(vec3,vec4[0]);
 			vec4[1]=_mm256_and_si256(vec3,vec4[1]);
 			vec4[2]=_mm256_and_si256(vec3,vec4[2]);
+			vec4[3]=_mm256_and_si256(vec3,vec4[3]);
 			__attribute__ ((aligned (32))) ullint out[4];
-			for(int c=0;c<3;++c){
+			for(int c=0;c<4;++c){
 				_mm256_store_si256((__m256i *)&out[0],vec4[c]);
 				if(out[0]) ceros--;
 				w[out[0]]++;
@@ -198,17 +205,10 @@ void HyperLogLog::estCard(){
 				if(out[3]) ceros--;
 				w[out[3]]++;
 			}
+			
 			++it1;
 		}
-		unsigned char tam=(long)N%12;
-		ullint i1=*it1;
-		for(char i=0;i<tam;++i){ //12 registros por celda
-			ullint temp1=i1&0x1F;
-			w[temp1]++;
-			i1=i1>>5;
-		}
-		//eliminamos las repeticiones por celda extra vacía creada (?)
-		w[0]-=12;
+
 		float card=0;
 		vector<float> w2;
 		for(int i=0;i<b+2;++i) w2.emplace_back((float)1/(float)(1<<i));
@@ -228,15 +228,16 @@ void HyperLogLog::estCard(){
 			card=-lim*log(1-(card/lim));
 		printf("estimacion cardinalidad %s: %f\n",names[j].c_str(),card);
 		cards[j]=card;
+
+		fclose(fp);
 	}
 	printf("\n");
 }
 
 void HyperLogLog::estJaccard(){ //recibe numero de genomas
-	//solo funciona con avx 512, por lineas _mm256_max_epu64()
-	/*__m256i vec3; //vector de mascaras de bits
-	ullint bits_and[4]={31,31,31,31};
-	vec3=_mm256_loadu_si256((const __m256i*)&bits_and[0]);*/
+	__m256i vec3; //vector de mascaras de bits
+	ullint bits_and[4]={15,15,15,15};
+	vec3=_mm256_loadu_si256((const __m256i*)&bits_and[0]);
 
 	int tam=sketch.size();
 	#pragma omp parallel for //no se puede usar collapse aqui
@@ -250,31 +251,36 @@ void HyperLogLog::estJaccard(){ //recibe numero de genomas
 			int ceros=N;
 			vector<float> wU(b+2,0);
 			//contamos las repeticiones de w en cada sketch
-			while(it1!=fin-1){ 
+			while(it1!=fin){ 
 				ullint i1=*it1,i2=*it2;
 
 				//solo funciona con avx 512, por lineas _mm256_max_epu64()
-				/*ullint it_array1[12]={i1,i1>>5,i1>>10,i1>>15,i1>>20,i1>>25,i1>>30,i1>>35,i1>>40,i1>>45,i1>>50,i1>>55};
-				ullint it_array2[12]={i2,i2>>5,i2>>10,i2>>15,i2>>20,i2>>25,i2>>30,i2>>35,i2>>40,i2>>45,i2>>50,i2>>55};
-				__m256i vec4[3],vec5[3];
+				/*ullint it_array1[16]={i1,(i1>>4),(i1>>8),(i1>>12),(i1>>16),(i1>>20),(i1>>24),(i1>>28),(i1>>32),(i1>>36),(i1>>40),(i1>>44),(i1>>48),(i1>>52),(i1>>56),(i1>>60)};
+				ullint it_array2[16]={i2,(i2>>4),(i2>>8),(i2>>12),(i2>>16),(i2>>20),(i2>>24),(i2>>28),(i2>>32),(i2>>36),(i2>>40),(i2>>44),(i2>>48),(i2>>52),(i2>>56),(i2>>60)};
+				__m256i vec4[4],vec5[4];
 				vec4[0]=_mm256_loadu_si256((const __m256i*)&it_array1[0]);
 				vec4[1]=_mm256_loadu_si256((const __m256i*)&it_array1[4]);
 				vec4[2]=_mm256_loadu_si256((const __m256i*)&it_array1[8]);
+				vec4[3]=_mm256_loadu_si256((const __m256i*)&it_array1[12]);
 				vec4[0]=_mm256_and_si256(vec3,vec4[0]);
 				vec4[1]=_mm256_and_si256(vec3,vec4[1]);
 				vec4[2]=_mm256_and_si256(vec3,vec4[2]);
+				vec4[3]=_mm256_and_si256(vec3,vec4[3]);
 				vec5[0]=_mm256_loadu_si256((const __m256i*)&it_array2[0]);
 				vec5[1]=_mm256_loadu_si256((const __m256i*)&it_array2[4]);
 				vec5[2]=_mm256_loadu_si256((const __m256i*)&it_array2[8]);
+				vec5[3]=_mm256_loadu_si256((const __m256i*)&it_array2[12]);
 				vec5[0]=_mm256_and_si256(vec3,vec5[0]);
 				vec5[1]=_mm256_and_si256(vec3,vec5[1]);
 				vec5[2]=_mm256_and_si256(vec3,vec5[2]);
+				vec5[3]=_mm256_and_si256(vec3,vec5[3]);
 
 				vec4[0]=_mm256_max_epu64(vec4[0],vec5[0]);
 				vec4[1]=_mm256_max_epu64(vec4[1],vec5[1]);
 				vec4[2]=_mm256_max_epu64(vec4[2],vec5[2]);
+				vec4[3]=_mm256_max_epu64(vec4[2],vec5[3]);
 				__attribute__ ((aligned (32))) ullint out[4];
-				for(char c=0;c<3;++c){
+				for(char c=0;c<4;++c){
 					_mm256_store_si256((__m256i *)&out[0],vec4[c]);
 					if(out[0]) ceros--;
 					wU[out[0]]++;
@@ -285,34 +291,22 @@ void HyperLogLog::estJaccard(){ //recibe numero de genomas
 					if(out[3]) ceros--;
 					wU[out[3]]++;
 				}*/
-				
-				//comentar este for y descomentar otras lineas si se posee arquitectura avx 512
-				for(char i=0;i<12;++i){ //12 registros por celda
-					ullint temp1=i1&0x1F,temp2=i2&0x1F;
+
+				for(char i=0;i<16;++i){ //16 registros por celda
+					ullint temp1=i1&0xF,temp2=i2&0xF;
 					if(temp1||temp2) --ceros;
 					(temp1>temp2) ? wU[temp1]++ : wU[temp2]++;
-					i1=i1>>5;
-					i2=i2>>5;
+					i1=i1>>4;
+					i2=i2>>4;
 				}
 				++it1;
 				++it2; //avanza en tabla B
 			}
-			unsigned char tam=(long)N%12;
-			ullint i1=*it1,i2=*it2;
-			for(char i=0;i<tam;++i){ //12 registros por celda
-				ullint temp1=i1&0x1F,temp2=i2&0x1F;
-				if(temp1||temp2) --ceros;
-				(temp1>temp2) ? wU[temp1]++ : wU[temp2]++;
-				i1=i1>>5;
-				i2=i2>>5;
-			}
-			//eliminamos las repeticiones por celda extra vacía creada (?)
-			wU[0]-=12;
 			
 			float cardU=0;
 
 			vector<float> w2;
-			for(int i=0;i<b+2;++i) w2.emplace_back((float)1/(float)(1<<i));
+			for(int i=0;i<min(b+2,17);++i) w2.emplace_back((float)1/(float)(1<<i));
 			__m256 vec,vec2;
 			for(int i=0;i<ciclos_red;++i){
 				vec=_mm256_loadu_ps((const float *)&wU[i*8]);
