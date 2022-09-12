d6b9a6d20d761fbffe32be6d79339e1b7a4d1b66
esalini22 <95315753+esalini22@users.noreply.github.com>
2022-09-12 14:16:59 -0300

    Update HyperLogLog.h

--- HyperLogLog.h
+++ HyperLogLog.h
@@ -14,12 +14,14 @@ class HyperLogLog{
         int ciclos_red; //cantidad de ciclos de for de reduce vectorizado
         string kmer_length;
         string sketch_size;
+        __m256 vect[4]; //vector wt vectorizado
         vector<ullint> bit_mask; //vector de máscaras de bits, usadas para insertar registros
         //se usa vector de ullint, cada celda (64 bits) a su vez tiene hasta 12 buckets
         vector<vector<ullint>> sketch; //n sketches, n: numero de genomas
         vector<string> names; //vector con los nombres de los genomas
         vector<float> cards; //cardinalidades individuales, para cálculo de jaccard
         vector<vector<float>> jaccards; //matriz de jaccards
+        unsigned int min_val; //minimo valor en v2 para no sobrepasar registro 15
     public:
         HyperLogLog(unsigned char n1,unsigned char n2,unsigned char k,int numThreads,int n);
         ~HyperLogLog();
