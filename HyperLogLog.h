4a96c68b15c6168fba87842aacd55f2f17abc915
esalini22 <95315753+esalini22@users.noreply.github.com>
2022-11-03 20:25:58 -0300

    Update HyperLogLog.h

--- HyperLogLog.h
+++ HyperLogLog.h
@@ -10,30 +10,26 @@ class HyperLogLog{
         unsigned char p,b;
         unsigned int bits_v2; //bits para hacer & en operacion bitwise
         long double N; //corresponde a |M|: numero de buckets - se usa long double para evitar hacer casting al calcular cardinalidad
-        float a_m; //factor de correccion * N^2
-        int ciclos_red; //cantidad de ciclos de for de reduce vectorizado
         string kmer_length;
         string sketch_size;
-        __m256 vect[4]; //vector wt vectorizado
         vector<ullint> bit_mask; //vector de máscaras de bits, usadas para insertar registros
         //se usa vector de ullint, cada celda (64 bits) a su vez tiene hasta 12 buckets
-        vector<vector<ullint>> sketch; //n sketches, n: numero de genomas
-        vector<string> names; //vector con los nombres de los genomas
-        vector<float> cards; //cardinalidades individuales, para cálculo de jaccard
-        vector<vector<float>> jaccards; //matriz de jaccards
+        vector<ullint> sketch; //n sketches, n: numero de genomas
+        string name; //vector con los nombres de los genomas
         unsigned int min_val; //minimo valor en v2 para no sobrepasar registro 15
     public:
-        HyperLogLog(unsigned char n1,unsigned char n2,unsigned char k,int numThreads,int n);
+        HyperLogLog(unsigned char n1,unsigned char n2,unsigned char k);
         ~HyperLogLog();
-        void addSketch(string genome,int i); //añade un sketch vacio al mapa de sketches, para funciones hll y dist
-        void insert(ullint kmer,int i); //inserta en sketch, para funciones hll y dist
-        void loadSketch(char* infilename,int i); //carga un archivo de sketch en memoria, para hll y dist
-        void saveSketches(); //guarda el sketch individual en un archivo, para funcion sketch
-        void saveOutput(char* filename); //guarda las cardinalidades y distancias en un txt
         inline float hsum_sse3(__m128 v);
         inline float hsum_avx(__m256 v);
-        void estCard(); //estima cardinalidad individual, para funciones hll y dist
-        void estJaccard(); //estima jaccard, para funcion dist
-        void printMatrix();
+        void addSketch(string genome); //añade un sketch vacio al mapa de sketches, para funciones hll y dist
+        void insert(ullint kmer); //inserta en sketch, para funciones hll y dist
+        void loadSketch(char* infilename); //carga un archivo de sketch en memoria, para hll y dist
+        void saveSketch(); //guarda el sketch individual en un archivo, para funcion sketch
+        float estCard(); //cardinalidad individual
+        vector<ullint> merge(HyperLogLog *hll); //hace la union y devuelve un nuevo sketch
+        vector<ullint> getSketch();
+        string getName();
+        //void saveOutput(char* filename); //guarda las cardinalidades y distancias en un txt
 };
 #endif
